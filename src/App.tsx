e19af73d3c0f7f352773159b4d4895f579c9bdd4
aserlite <arthurcuvillon691@gmail.com>
2026-06-09 17:14:47 +0100

    cache last page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import { ThemeProvider } from './contexts/ThemeContext';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
@@ -8,6 +9,39 @@ import Me from './pages/Me';
 import Contact from './pages/Contact';
 
 import './themes/classic/classic.css';
+
+function RouteCache() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isFirstLoad = useRef(true);
+
+  useEffect(() => {
+    if (isFirstLoad.current) {
+      const savedPath = localStorage.getItem('lastPath');
+      if (savedPath && savedPath !== '/' && location.pathname === '/') {
+        navigate(savedPath, { replace: true });
+      }
+      isFirstLoad.current = false;
+    } else {
+      localStorage.setItem('lastPath', location.pathname);
+    }
+  }, [location.pathname, navigate]);
+
+  useEffect(() => {
+    const savedScroll = sessionStorage.getItem(`scroll-${location.key}`);
+    if (savedScroll) {
+      setTimeout(() => window.scrollTo(0, parseInt(savedScroll, 10)), 10);
+    } else {
+      window.scrollTo(0, 0);
+    }
+
+    return () => {
+      sessionStorage.setItem(`scroll-${location.key}`, window.scrollY.toString());
+    };
+  }, [location.pathname, location.key]);
+
+  return null;
+}
 
 function NotFound() {
   return (
@@ -24,6 +58,7 @@ function App() {
   return (
     <ThemeProvider>
       <BrowserRouter basename={basename}>
+        <RouteCache />
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
